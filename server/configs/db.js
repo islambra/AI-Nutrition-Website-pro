2c5d1a0200b4a5b29c2e68c551e4f0e94a5d921c
islambra <mohamedbrahamia25@gmail.com>
2026-07-12 21:24:16 +0100

    fix

--- server/configs/db.js
+++ server/configs/db.js
@@ -11,9 +11,7 @@ const connectDB = async () => {
       maxPoolSize: 20,
       minPoolSize: 5,
       serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 45000,
-      keepAlive: true,
-      keepAliveInitialDelay: 300000
+      socketTimeoutMS: 45000
     });
   } catch (error) {
     console.error("Database connection error:", error.message);
